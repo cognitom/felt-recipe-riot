ebad2d726225f0b95c46b1de61152d0d521fd94a
Tsutomu Kawamura <cognitom@gmail.com>
2016-07-29 12:39:26 +0900

    autoprefixer --> postcss-cssnext

--- felt.config.js
+++ felt.config.js
@@ -12,7 +12,7 @@ const
   commonjs = require('rollup-plugin-commonjs'),
   postcss = require('felt-postcss'),
   postcssImport = require('postcss-import'),
-  autoprefixer = require('autoprefixer')
+  cssnext = require('postcss-cssnext')
 
 module.exports = {
   // default handlers for each extension
@@ -31,7 +31,7 @@ module.exports = {
     '.css': postcss({
       plugins: [
         postcssImport(),
-        autoprefixer()
+        cssnext()
       ],
       options: {
         map: { inline: false }
